e1de40a2bdcd678117d7a267f81c0338e89cebb9
fronte-finem <fronte.finem+github@ya.ru>
2021-07-29 16:24:06 +0300

    feat: add entry file

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import '@assets/styles/reset.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { App } from './app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
