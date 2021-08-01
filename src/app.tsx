0e1eca7660885e75550a0fd54ca4b7cd5b221563
fronte-finem <fronte.finem+github@ya.ru>
2021-08-01 23:44:36 +0300

    refactor: change data source for app

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,15 @@
-import { css } from '@linaria/core';
+import { css, cx } from '@linaria/core';
 import React from 'react';
-import { Card } from 'components/card/card';
-import { getData } from 'services/data/data-sample';
+import { Card, factionTheme } from 'components/card/card';
+import { getCreatures } from 'services/data/creature-sample';
 
 const app = css`
   max-width: 1200px;
   width: 100%;
   margin: auto;
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  grid-gap: 10px;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 20px;
   justify-content: center;
   justify-items: center;
   padding: 20px;
@@ -17,9 +17,9 @@ const app = css`
 
 export const App: React.FC = () => {
   return (
-    <div className={app}>
-      {getData().map((item) => (
-        <Card key={item.id} data={item} />
+    <div className={cx(app, factionTheme)}>
+      {getCreatures().map((creature) => (
+        <Card key={creature.name} creature={creature} />
       ))}
     </div>
   );
