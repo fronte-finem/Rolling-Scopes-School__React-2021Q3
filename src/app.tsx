1e80d993399151304788681a220f933243db841e
fronte-finem <fronte.finem+github@ya.ru>
2021-08-03 23:29:01 +0300

    refactor: build app layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,21 @@
-import { css, cx } from '@linaria/core';
 import React from 'react';
-import { Card, factionTheme } from 'components/card/card';
+import { css } from '@linaria/core';
+import { SearchBar } from 'components/search-bar/search-bar';
+import { CardsGrid } from 'components/cards-grid/cards-grid';
 import { getCreatures } from 'services/data/creature-sample';
 
-const app = css`
-  max-width: 1200px;
+const appContainer = css`
+  max-width: 1400px;
   width: 100%;
-  margin: auto;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  grid-gap: 20px;
-  justify-content: center;
-  justify-items: center;
-  padding: 20px;
+  margin: 0 auto;
+  background: #0008;
 `;
 
 export const App: React.FC = () => {
   return (
-    <div className={cx(app, factionTheme)}>
-      {getCreatures().map((creature) => (
-        <Card key={creature.name} creature={creature} />
-      ))}
+    <div className={appContainer}>
+      <SearchBar />
+      <CardsGrid creatures={getCreatures()} />
     </div>
   );
 };
