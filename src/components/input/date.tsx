494d31558e7ff6fd176ae5a07ea025f7644f515c
fronte-finem <fronte.finem+github@ya.ru>
2021-08-06 23:03:27 +0300

    refactor: change date-input props

--- src/components/input/date.tsx
+++ src/components/input/date.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-// import classes from './date.module.css';
-import { ErrorProps, InputWrapper } from './wrapper';
+import classes from './date.module.css';
+import { InputWrapper } from './wrapper';
+import { ErrorProps, InputStaticProps } from './types';
 
-export const dateToString = (date: Date) => date.toISOString().split('T')[0];
-
-interface InputDateProps {
-  id: string;
-  name?: string;
-  label: string;
-  onInput: (value: Date | null) => void;
-  value: string;
+export interface InputDateStaticProps extends InputStaticProps, ErrorProps {
   min?: string;
   max?: string;
-  error?: ErrorProps;
+}
+
+export interface InputDateProps extends InputDateStaticProps {
+  onInput: (value: Date | null) => void;
+  onInvalid: () => void;
+  value: string;
 }
 
 export const InputDate: React.FC<InputDateProps> = ({
@@ -20,26 +19,27 @@ export const InputDate: React.FC<InputDateProps> = ({
   name,
   label,
   onInput,
+  onInvalid,
   value,
   min,
   max,
-  error,
+  isError,
+  errorMessage,
+  required,
 }) => {
   const handleInput = (event: React.FormEvent<HTMLInputElement>) => {
     onInput(event.currentTarget.valueAsDate);
   };
 
   return (
-    <InputWrapper id={id} label={label} error={error}>
+    <InputWrapper {...{ id, label, errorMessage, isError }}>
       <input
+        className={`${classes.date} ${classes.clickable} ${classes.half}`}
         type="date"
-        id={id}
+        {...{ id, min, max, value, required }}
         name={name || id}
         onInput={handleInput}
-        value={value}
-        min={min}
-        max={max}
-        required
+        onInvalid={onInvalid}
         pattern="\d{4}-\d{2}-\d{2}"
       />
     </InputWrapper>
