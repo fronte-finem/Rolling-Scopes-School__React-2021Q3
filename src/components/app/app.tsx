029ac805c14299233658c2a2e05155e5ff2b28d3
fronte-finem <fronte.finem+github@ya.ru>
2021-08-06 23:14:43 +0300

    refactor: manage state between main-components in app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PersonForm } from 'components/person-form/person-form';
 import classes from './app.module.css';
+import { PersonFormData } from '../person-form/state';
+import { PersonCardsField } from '../person-cards-field/person-cards-field';
 
 export const App = () => {
+  const [dataArray, setDataArray] = useState<PersonFormData[]>([]);
+
+  const handleSubmit = (data: PersonFormData) => {
+    setDataArray((prev) => [...prev, data]);
+  };
+
   return (
     <div className={classes.app}>
-      <PersonForm />
+      <div className={classes.container}>
+        <div className={classes.containerForm}>
+          <PersonForm onSubmit={handleSubmit} />
+        </div>
+        <div className={classes.containerField}>
+          <PersonCardsField dataArray={dataArray} />
+        </div>
+      </div>
     </div>
   );
 };
