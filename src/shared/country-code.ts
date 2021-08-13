ef22142e58283967b25cb9b9d08065e58d8361f8
fronte-finem <fronte.finem+github@ya.ru>
2021-08-13 21:22:36 +0300

    feat: add country-codes mapping for api-enum

---
+++ src/shared/country-code.ts
@@ -0,0 +1,8 @@
+/** ISO 3166-1 alpha-2 country code */
+
+export const CountryCodeMap: Map<string, string> = new Map([
+  ['JP', 'japan'],
+  ['KR', 'south-korea'],
+  ['CN', 'china'],
+  ['TW', 'taiwan'],
+]);
