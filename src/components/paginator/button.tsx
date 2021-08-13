696496f74b2cf6afa18ce3c7df1ec5d902c8c996
fronte-finem <fronte.finem+github@ya.ru>
2021-08-13 21:36:38 +0300

    feat: add custom buttons for page-selector

---
+++ src/components/paginator/button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './button.module.pcss';
+
+export const Button: React.FC<{
+  page: number;
+  disabled?: boolean;
+  onClick: (page: number) => void;
+}> = ({ page, disabled, onClick }) => {
+  return (
+    <button
+      type="button"
+      className={classes.button}
+      disabled={disabled}
+      onClick={() => onClick(page)}>
+      {page}
+    </button>
+  );
+};
