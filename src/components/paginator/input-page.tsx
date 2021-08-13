16ac2c748fcc6218d2683ee20ede9d91c871a978
fronte-finem <fronte.finem+github@ya.ru>
2021-08-13 21:37:26 +0300

    feat: add component for jump on any page

---
+++ src/components/paginator/input-page.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import classes from './input-page.module.pcss';
+
+export const InputPage: React.FC<{
+  page: number;
+  last: number;
+  onChange: (page: number) => void;
+}> = ({ page, last, onChange }) => {
+  const [value, setValue] = useState(page);
+
+  const handleInput = (event: React.FormEvent<HTMLInputElement>) => {
+    let nextPage = +event.currentTarget.value;
+    nextPage = nextPage > last ? last : nextPage;
+    nextPage = nextPage < 1 ? 1 : nextPage;
+    setValue(Math.ceil(nextPage));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onChange(value);
+  };
+
+  return (
+    <form className={classes.form} onSubmit={handleSubmit}>
+      <div className={classes.form}>
+        <input
+          className={classes.input}
+          type="number"
+          value={value}
+          min={1}
+          max={last}
+          step={1}
+          required
+          onInput={handleInput}
+        />
+        <button
+          type="submit"
+          className={classes.submit}
+          disabled={value === page}>
+          {value !== page && 'go'}
+        </button>
+      </div>
+    </form>
+  );
+};
