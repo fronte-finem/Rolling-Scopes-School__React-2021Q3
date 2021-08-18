c0b3b477322588cfb61c542cbc5844fa24f254d3
fronte-finem <fronte.finem+github@ya.ru>
2021-08-18 21:06:32 +0300

    refactor: move getter

--- src/shared/country-code.ts
+++ src/shared/country-code.ts
@@ -1,8 +1,12 @@
 /** ISO 3166-1 alpha-2 country code */
 
+import { mapGetter } from './maybe';
+
 export const CountryCodeMap: Map<string, string> = new Map([
   ['JP', 'japan'],
   ['KR', 'south-korea'],
   ['CN', 'china'],
   ['TW', 'taiwan'],
 ]);
+
+export const getCountry = mapGetter(CountryCodeMap);
