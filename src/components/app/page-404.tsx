26bf8f779ecef8adef424b9e04236d90387fd72c
fronte-finem <fronte.finem+github@ya.ru>
2021-08-18 21:35:10 +0300

    feat: add pages 404 and test

---
+++ src/components/app/page-404.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './page-404.module.pcss';
+
+export function Page404() {
+  return (
+    <div className={classes.page404}>
+      <h2 className={classes.title}>404</h2>
+    </div>
+  );
+}
---
+++ src/components/app/page-test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './page-test.module.pcss';
+
+export function PageTest() {
+  return (
+    <div className={classes.pageTest}>
+      <h2 className={classes.title}>Test</h2>
+    </div>
+  );
+}
