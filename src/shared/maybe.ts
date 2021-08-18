cc99051f0781248cb4ac4b5052de31bd4a80dbbb
fronte-finem <fronte.finem+github@ya.ru>
2021-08-18 21:05:27 +0300

    refactor: more type-safe work with Maybe

--- src/shared/maybe.ts
+++ src/shared/maybe.ts
@@ -1,11 +1,12 @@
-export type Maybe<T> = T | null | undefined;
+export type None = null | undefined;
+export type Maybe<T> = T | None;
 
-export function none<T>(maybeT: Maybe<T>): boolean {
+export function isNone<T>(maybeT: Maybe<T>): maybeT is None {
   return maybeT === undefined || maybeT === null;
 }
 
-export function just<T>(maybeT: Maybe<T>): boolean {
-  return !none(maybeT);
+export function isValue<T>(maybeT: Maybe<T>): maybeT is T {
+  return !isNone(maybeT);
 }
 
 export type Op<A, B> = (a: A) => B;
@@ -13,28 +14,31 @@ export type OpMaybe<A, B> = (maybeA: Maybe<A>) => Maybe<B>;
 export type GetMaybe<A, B> = (a: A) => Maybe<B>;
 
 export function mapGetter<A, B>(mapAB: Map<A, B>): OpMaybe<A, B> {
-  return (maybeA: Maybe<A>) => maybeA && mapAB.get(maybeA);
+  return (maybeA: Maybe<A>) => (isNone(maybeA) ? maybeA : mapAB.get(maybeA));
 }
 
 export class MaybeUtils {
   static funcMap<A, B>(func: Op<A, B>): OpMaybe<A, B> {
-    return (maybeA: Maybe<A>): Maybe<B> => maybeA && func(maybeA);
+    return (maybeA: Maybe<A>): Maybe<B> =>
+      isNone(maybeA) ? maybeA : func(maybeA);
   }
 
   static varMap<A, B>(maybeA: Maybe<A>): (op: Op<A, B>) => Maybe<B> {
-    return (func: Op<A, B>): Maybe<B> => maybeA && func(maybeA);
+    return (func: Op<A, B>): Maybe<B> =>
+      isNone(maybeA) ? maybeA : func(maybeA);
   }
 
   static strictMap<A, B>(maybeA: Maybe<A>, func: Op<A, B>): Maybe<B> {
-    return maybeA && func(maybeA);
+    return isNone(maybeA) ? maybeA : func(maybeA);
   }
 
   static apply<A, B>(maybeFunc: Maybe<Op<A, B>>): OpMaybe<A, B> {
     return (maybeA: Maybe<A>): Maybe<B> =>
-      maybeFunc && MaybeUtils.funcMap(maybeFunc)(maybeA);
+      isNone(maybeFunc) ? maybeFunc : MaybeUtils.funcMap(maybeFunc)(maybeA);
   }
 
   static bind<A, B>(getMaybe: GetMaybe<A, B>): OpMaybe<A, B> {
-    return (maybeA: Maybe<A>): Maybe<B> => maybeA && getMaybe(maybeA);
+    return (maybeA: Maybe<A>): Maybe<B> =>
+      isNone(maybeA) ? maybeA : getMaybe(maybeA);
   }
 }
