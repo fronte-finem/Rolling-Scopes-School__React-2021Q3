cd55f917df9c82889afc7f314bd6b360ac661db2
fronte-finem <fronte.finem+github@ya.ru>
2021-08-18 21:11:52 +0300

    refactor: extract svg-sprites components

---
+++ src/components/svg/sprites.tsx
@@ -0,0 +1,7 @@
+import iconsSprite from 'assets/sprites/icons.svg';
+import flagsSprite from 'assets/sprites/flags.svg';
+import { withSprite } from './svg';
+
+export const Icon = withSprite(iconsSprite);
+
+export const Flag = withSprite(flagsSprite);
