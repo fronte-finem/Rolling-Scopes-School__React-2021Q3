a873343d9d8198c1d546246646800cf9a4237c46
fronte-finem <fronte.finem+github@ya.ru>
2021-08-22 20:29:57 +0300

    refactor: extract props interface

--- src/components/shared/stats/stats.tsx
+++ src/components/shared/stats/stats.tsx
@@ -3,12 +3,15 @@ import { Maybe } from 'shared/maybe';
 import { Info } from 'components/shared/info/info';
 import classes from './stats.module.pcss';
 
-export const Stats: React.FC<{
-  mediaStats: { popularity?: Maybe<number>; meanScore?: Maybe<number> };
-}> = ({ mediaStats: { meanScore, popularity } }) => {
+interface StatsProps {
+  popularity?: Maybe<number>;
+  score?: Maybe<number>;
+}
+
+export const Stats: React.FC<StatsProps> = ({ popularity, score }) => {
   return (
     <div className={classes.stats}>
-      {meanScore && <Info info={meanScore / 10} icon="star" />}
+      {score && <Info info={score / 10} icon="star" />}
       {popularity && <Info info={popularity} icon="people" />}
     </div>
   );
