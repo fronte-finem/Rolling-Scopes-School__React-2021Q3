e62a88d82425d24b0e8ea0d9b786ce9b76c9951a
fronte-finem <fronte.finem+github@ya.ru>
2021-08-22 15:31:23 +0300

    refactor: remove utility-state for details-page

--- src/components/app/page-details.tsx
+++ src/components/app/page-details.tsx
@@ -4,47 +4,32 @@ import { FullLoader } from 'components/loader/full-loader';
 import { ErrorInfo } from 'components/error-info/error-info';
 import { Media } from 'components/media/media';
 import { updateTitle } from 'shared/update-title';
-import { useAppDispatch, useAppSelector } from 'store/hooks/hooks';
-import { detailsFetch, reset, setId } from 'store/slices/detailsSlice';
+import { useDetailsQuery } from 'services/anilist-api/anilist-api';
 import classes from './page-details.module.pcss';
 
 export function PageDetails() {
   const { id } = useParams<{ id: string }>();
-  const dispatch = useAppDispatch();
-  const details = useAppSelector((state) => state.details);
+  const { isLoading, data, isError, error } = useDetailsQuery({ id: +id });
 
   React.useEffect(() => {
-    if (+id === details.id && details.data) return () => {};
-    if (+id !== details.id) dispatch(reset());
-
-    const promise = dispatch(detailsFetch({ id: +id }));
-
-    return () => {
-      if (details.isLoading) promise.abort();
-      dispatch(setId(+id));
-    };
-  }, []);
-
-  React.useEffect(() => {
-    if (details.isLoading || details.isError) return;
-    if (details.data?.Media?.title?.romaji)
-      updateTitle(details.data.Media.title.romaji);
-  }, [details]);
+    if (isLoading || isError) return;
+    if (data?.Media?.title?.romaji) updateTitle(data.Media.title.romaji);
+  }, [isLoading, isError, data]);
 
   return (
     <div className={classes.pageDetails}>
-      {details.isLoading && <FullLoader />}
-      {details.isError && (
+      {isLoading && <FullLoader />}
+      {isError && (
         <div className={classes.errorWrapper}>
-          <ErrorInfo error={details.error} />
+          <ErrorInfo error={error} />
         </div>
       )}
-      {!details.isLoading && !details.data && (
+      {!isLoading && !data && (
         <div className={classes.messageWrapper}>
           （＞人＜；） No results for ID: &quot;{id}&quot;
         </div>
       )}
-      {details.data?.Media && <Media media={details.data.Media} />}
+      {data?.Media && <Media media={data.Media} />}
     </div>
   );
 }
