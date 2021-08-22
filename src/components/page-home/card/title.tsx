9d563041ba6e8d6536ef6485886056ed3f2fdb21
fronte-finem <fronte.finem+github@ya.ru>
2021-08-22 20:31:32 +0300

    refactor: split props for title

--- src/components/page-home/card/title.tsx
+++ src/components/page-home/card/title.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { TitleFragment } from 'services/anilist-api/generated/search-query-types';
 import { Info } from 'components/shared/info/info';
+import { Maybe } from 'shared/maybe';
 import classes from './title.module.pcss';
-import { MediaProps } from './types';
 
-const formatTip = (title: TitleFragment) =>
-  `${title.romaji || ''}\n${title.english || ''}\n${title.native || ''}`;
+interface TitleProps {
+  title: Maybe<TitleFragment>;
+  studio?: string;
+}
 
-export const Title: React.FC<MediaProps> = ({ media }) => {
-  if (!media.title) return <></>;
+const selectTitle = (title: TitleFragment) => {
+  return title.english || title.romaji || title.native;
+};
+
+export const Title: React.FC<TitleProps> = ({ title, studio }) => {
+  if (!title) return <></>;
   return (
-    <div className={classes.header} title={formatTip(media.title)}>
-      <div className={classes.title}>{media.title.romaji}</div>
-      <div className={classes.studio}>
-        {media.studios?.nodes?.[0] && (
-          <Info info={media.studios.nodes[0].name} icon="home" />
-        )}
-      </div>
+    <div className={classes.header}>
+      <div className={classes.title}>{selectTitle(title)}</div>
+      {studio && (
+        <div className={classes.studio}>
+          <Info info={studio} icon="home" />
+        </div>
+      )}
     </div>
   );
 };
