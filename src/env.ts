45d23d00c72a9909b0bf9aceee6c19ccbf4feaa8
fronte-finem <fronte.finem+github@ya.ru>
2021-08-30 10:11:48 +0300

    refactor: remove env logs

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,3 @@ declare const ENV_TITLE: string;
 export const IS_PROD: boolean = ENV_IS_PROD;
 export const PUBLIC_URL: string = ENV_PUBLIC_URL;
 export const TITLE: string = ENV_TITLE;
-
-console.log(`env:IS_PROD<${typeof IS_PROD}> = `, IS_PROD);
-console.log(`env:PUBLIC_URL<${typeof PUBLIC_URL}> = `, PUBLIC_URL);
-console.log(`env:TITLE<${typeof TITLE}> = `, TITLE);
