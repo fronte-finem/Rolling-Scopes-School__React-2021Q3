d9c63ce9864e2321bd90586d707cf5044c5bb41e
fronte-finem <fronte.finem+github@ya.ru>
2021-08-30 10:12:34 +0300

    refactor: move store creation to function

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,17 +3,20 @@ import { configureStore } from '@reduxjs/toolkit';
 import { anilistApi } from 'services/anilist-api/anilist-api';
 import { searchVarsReducer } from './slices/search-vars';
 
-export const store = configureStore({
-  reducer: {
-    searchVars: searchVarsReducer,
-    [anilistApi.reducerPath]: anilistApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) => [
-    ...getDefaultMiddleware(),
-    anilistApi.middleware,
-  ],
-  devTools: !IS_PROD,
-});
+export const getStore = () =>
+  configureStore({
+    reducer: {
+      searchVars: searchVarsReducer,
+      [anilistApi.reducerPath]: anilistApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => [
+      ...getDefaultMiddleware(),
+      anilistApi.middleware,
+    ],
+    devTools: !IS_PROD,
+  });
+
+export const store = getStore();
 
 export type RootState = ReturnType<typeof store.getState>;
 
