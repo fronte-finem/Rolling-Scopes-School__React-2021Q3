ef044f164cc23b2c041c755cf846f7e1f6a6787a
fronte-finem <fronte.finem+github@ya.ru>
2021-08-30 10:10:36 +0300

    refactor: extract root element

--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,20 @@ import { App } from 'components/app/app';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
+export const Root = (): JSX.Element => (
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
+
 const initApp = () => {
   const root = document.createElement('div');
   root.id = 'root';
   document.body.append(root);
 
-  ReactDOM.render(
-    <Provider store={store}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Provider>,
-    root
-  );
+  ReactDOM.render(<Root />, root);
 };
 
 initApp();
