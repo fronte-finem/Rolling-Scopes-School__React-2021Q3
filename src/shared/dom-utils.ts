3d238dfc5f114aa4130e115fe70ff30cfc336362
fronte-finem <fronte.finem+github@ya.ru>
2021-08-30 10:15:28 +0300

    refactor: rename to dom-utils

--- src/shared/dom-utils.ts
+++ src/shared/dom-utils.ts
@@ -6,3 +6,7 @@ const TITLE_SPLITTER = ' | ';
 export const updateTitle = (title?: Maybe<string>) => {
   document.title = title ? `${TITLE}${TITLE_SPLITTER}${title}` : TITLE;
 };
+
+export const toggleParentClass =
+  (className: string, enable: boolean) => (node: HTMLElement) =>
+    node.parentElement?.classList.toggle(className, enable);
