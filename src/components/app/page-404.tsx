51631e78fc2453dc3f49906afb371eb3e7312fd6
fronte-finem <fronte.finem+github@ya.ru>
2021-10-25 19:56:42 +0300

    feat: add description to 404-error page

--- src/components/app/page-404.tsx
+++ src/components/app/page-404.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import classes from './page-404.module.pcss';
 
 export function Page404() {
+  const { current } = useRef(window.location.toString());
   return (
     <div className={classes.page404}>
-      <h2 className={classes.title}>404</h2>
+      <div>
+        <h2 className={classes.title}>404</h2>
+        <p className={classes.text}>Page &ldquo;{current}&rdquo; not exist;</p>
+      </div>
     </div>
   );
 }
